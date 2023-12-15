d9b17b84c6753fc77ea0147d55930bf2e345bd94
Vanessa Kasminoff <sunnset07@gmail.com>
2023-12-14 22:16:23 -0800

    Created a pressed keys variable that stores an array, refactored the keydown and keyup event listeners to add/remove the keys pressed respectively, while determining direction of keys still pressed, refactored code to allow for both WASD and arrow keys player input for movement

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
+function tileImg(url, top, left, width, height) {
+  let bgDiv = document.createElement("div");
+  bgDiv.style.backgroundImage = `url('${url}')`;
+  bgDiv.style.backgroundRepeat = "repeat";
+  bgDiv.style.position = "fixed";
+  bgDiv.style.top = top;
+  bgDiv.style.left = left;
+  bgDiv.style.width = width;
+  bgDiv.style.height = height;
+  document.body.append(bgDiv);
+}
+
+tileImg("assets/sky.png", "0px", "0px", "100%", "48%");
+tileImg("assets/grass.png", "48%", "0px", "100%", "67%");
+
 // Create the playable character
-const pc = newPlayableCharacter(100, 110)
+const pc = newPlayableCharacter(100, 110);
 
 // Create a non-playable character
-const npc = newNonPlayableCharacter(50, 300)
+const npc = newNonPlayableCharacter(50, 300);
 
 // have the NPC start walking east immediately
 async function moveNPC() {
-    await npc.walkNorth(1400)
-    await npc.walkEast(1200)
-    await npc.walkSouth(300)
-    await npc.walkEast(1500)
-    await npc.walkSouth(1500)
-    await npc.walkWest(2700)
-    await npc.walkNorth(1400)
+  await npc.walkNorth(1400);
+  await npc.walkEast(1200);
+  await npc.walkSouth(300);
+  await npc.walkEast(1500);
+  await npc.walkSouth(1500);
+  await npc.walkWest(2700);
+  await npc.walkNorth(1400);
 }
-moveNPC()
+moveNPC();
 // Create the inventory
-const inventory = newInventory()
-move(inventory).to(0, 0)
+const inventory = newInventory();
+move(inventory).to(0, 0);
 
 // Create everything else
-move(newImage('assets/tree.png')).to(200, 450)
-move(newImage('assets/pillar.png')).to(350, 250)
-move(newImage('assets/pine-tree.png')).to(450, 350)
-move(newImage('assets/crate.png')).to(150, 350)
-move(newImage('assets/well.png')).to(500, 575)
-move(newItem('assets/sword.png')).to(500, 555)
-move(newItem('assets/shield.png')).to(165, 335)
-move(newItem('assets/staff.png')).to(600, 250)
+move(newImage("assets/tree.png")).to(200, 450);
+move(newImage("assets/pillar.png")).to(350, 250);
+move(newImage("assets/pine-tree.png")).to(450, 350);
+move(newImage("assets/crate.png")).to(150, 350);
+move(newImage("assets/well.png")).to(500, 575);
+move(newItem("assets/sword.png")).to(500, 555);
+move(newItem("assets/shield.png")).to(165, 335);
+move(newItem("assets/staff.png")).to(600, 250);
--- move.js
+++ move.js
@@ -1,64 +1,82 @@
 function move(element) {
-    element.style.position = 'fixed'
+  element.style.position = "fixed";
 
-    function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
+  function moveToCoordinates(left, bottom) {
+    element.style.left = left + "px";
+    element.style.bottom = bottom + "px";
+  }
+
+  function moveWithArrowKeys(left, bottom, callback) {
+    let direction = null;
+    let x = left;
+    let y = bottom;
+
+    element.style.left = x + "px";
+    element.style.bottom = y + "px";
+
+    function moveCharacter() {
+      if (direction === "west") {
+        x--;
+      }
+      if (direction === "north") {
+        y++;
+      }
+      if (direction === "east") {
+        x++;
+      }
+      if (direction === "south") {
+        y--;
+      }
+      element.style.left = x + "px";
+      element.style.bottom = y + "px";
     }
 
-    function moveWithArrowKeys(left, bottom, callback){
-        let direction = null;
-        let x = left;
-        let y = bottom;
+    setInterval(moveCharacter, 1);
 
-        element.style.left = x + 'px'
-        element.style.bottom = y + 'px'
-        
-        function moveCharacter(){ 
-            if(direction === 'west'){
-                x-=1
-            }
-            if(direction === 'north'){
-                y+=1
-            }
-            if(direction === 'east'){
-                x+=1
-            }
-            if(direction === 'south'){
-                y-=1
-            }
-            element.style.left = x + 'px'
-            element.style.bottom = y + 'px'
-        }
-        
-        setInterval(moveCharacter, 1)
-        
-        document.addEventListener('keydown', function(e){
-            if(e.repeat) return;
-        
-            if(e.key === 'ArrowLeft'){
-                direction = 'west'
-            }
-            if(e.key === 'ArrowUp'){
-                direction = 'north'
-            }
-            if(e.key === 'ArrowRight'){
-                direction = 'east'
-            }
-            if(e.key === 'ArrowDown'){
-                direction = 'south'
-            }
-            callback(direction)
-        })
-        
-        document.addEventListener('keyup', function(e){
-            direction = null
-            callback(direction)
-        })
-    }
+    // Declare an array for the pressed keys
+    let pressedKeys = [];
 
-    return {
-        to: moveToCoordinates,
-        withArrowKeys: moveWithArrowKeys
+    // Adds the pressed key(s) to the array
+    document.addEventListener("keydown", function (e) {
+      if (e.repeat || pressedKeys.includes(e.key)) return;
+      pressedKeys.push(e.key);
+      // Invokes the updateDirection function to determine direction of keys pressed.
+      updateDirection();
+    });
+
+    document.addEventListener("keyup", function (e) {
+      // Finds the index of the released key in the array and stores it in the variable named index. If the released key is not in the array,
+      // it will return the value -1.
+      let index = pressedKeys.indexOf(e.key);
+      // Below logic checks if the released key is in the array by checking index.
+      if (index !== -1) {
+        // Removes the released key from the array
+        pressedKeys.splice(index, 1);
+        // Invokes the updateDirection function to determine direction of keys still pressed
+        updateDirection();
+      }
+    });
+
+    function updateDirection() {
+      direction = null;
+      if (pressedKeys.includes("ArrowLeft") || pressedKeys.includes("a")) {
+        direction = "west";
+      }
+      if (pressedKeys.includes("ArrowUp") || pressedKeys.includes("w")) {
+        direction = "north";
+      }
+      if (pressedKeys.includes("ArrowRight") || pressedKeys.includes("d")) {
+        direction = "east";
+      }
+      if (pressedKeys.includes("ArrowDown") || pressedKeys.includes("s")) {
+        direction = "south";
+      }
+      callback(direction);
     }
+  }
+
+  return {
+    to: moveToCoordinates,
+    withArrowKeys: moveWithArrowKeys,
+  };
 }
